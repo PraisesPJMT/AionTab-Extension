323178ac163bd0f205abe3ed63d2c82a5c6c9042
praisespjmt <praisesmusa@outlook.com>
2024-05-08 13:21:31 +0100

    add Constants module

---
+++ scripts/constants.js
@@ -0,0 +1,20 @@
+/**
+ * This module exports constants used throughout the codebase.
+ *
+ * @module constants
+ */
+
+/**
+ * The key for storing and retrieving the user's text preference from local storage.
+ *
+ * @type {string}
+ */
+export const USER_TEXT_OPT = 'userText';
+
+/**
+ * The key for storing and retrieving the user's search engine preference from local storage.
+ *
+ * @type {string}
+ */
+export const SEARCH_ENGINE_OPT = 'searchEngine';
+
